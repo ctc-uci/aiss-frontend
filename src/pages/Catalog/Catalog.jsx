af58be632ed886a88f9bd4cfc0a767b70e598e16
Sean Fong <seancfong@gmail.com>
2024-01-21 03:06:20 +0000

    Refactor code in Catalog component

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,65 +1,45 @@
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableContainer,
-  TableCaption,
-} from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 
 export default function Catalog() {
-  // Create data state
   const [tableData, setTableData] = useState([]);
 
-  // Fetch Table Data: .../catalog
   useEffect(() => {
-    fetch('http://localhost:3001/catalog')
-      .then(response => response.json())
-      .then(data => setTableData(data));
+    const fetchCatalogData = async () => {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_HOST}/catalog`);
+      const data = await response.json();
+      setTableData(data);
+    };
+
+    fetchCatalogData().catch(console.error);
   }, []);
 
-  // Render table data
   return (
-    <>
-      <TableContainer>
-        <Table variant="simple">
-          <TableCaption>Event Information</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Title</Th>
-              <Th>Host</Th>
-              <Th>Cohort Type</Th>
-              <Th>Event Type</Th>
-              <Th>Subject</Th>
-              <Th>Description</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {tableData.map(({ id, host, title, eventType, subject, description, year }) => (
-              <Tr key={id}>
-                <Td>{title}</Td>
-                <Td>{host}</Td>
-                <Td>{year}</Td>
-                <Td>{eventType}</Td>
-                <Td>{subject}</Td>
-                <Td>{description}</Td>
-              </Tr>
-            ))}
-          </Tbody>
-          <Tfoot>
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
             <Th>Title</Th>
             <Th>Host</Th>
             <Th>Cohort Type</Th>
             <Th>Event Type</Th>
             <Th>Subject</Th>
             <Th>Description</Th>
-          </Tfoot>
-        </Table>
-      </TableContainer>
-    </>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {tableData.map(({ id, host, title, eventType, subject, description, year }) => (
+            <Tr key={id}>
+              <Td>{title}</Td>
+              <Td>{host}</Td>
+              <Td>{year}</Td>
+              <Td>{eventType}</Td>
+              <Td>{subject}</Td>
+              <Td>{description}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 }
