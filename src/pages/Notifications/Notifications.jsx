1cd14c370b575b61f7a13f74d0a176ee1d1fe3db
Kristen Yee <kristenyee@dhcp-v111-128.mobile.uci.edu>
2024-01-26 11:08:19 -0800

    completed basic ui structure without accept/decline functionality

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,8 +1,62 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Link,
+} from '@chakra-ui/react'
+import { useEffect, useState } from 'react';
+import { NPOBackend } from '../../utils/auth_utils';
+
 const Notifications = () => {
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    const getPendingUsers = async () => {
+      const response = await NPOBackend.get(`/users/pending-accounts`);
+      setListData(response.data);
+    };
+    getPendingUsers().catch(console.error);
+  }, []);
+
+  // const Approve = async (id) => {
+  //   const response = await NPOBackend.put(`/users/approve/${id}`);
+  // }
+  // Approve().catch(console.error);
+
+  // const Decline = async (id) => {
+  //   const response = await NPOBackend.delete(`/users/${id}`);
+  // }
+  // Decline().catch(console.error);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+      <TableContainer>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Email</Th>
+              <Th>Accept</Th>
+              <Th>Decline</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {listData.map(({ id, email }) => (
+              <Tr key={id}>
+                <Td>{id}</Td>
+                <Td>{email}</Td>
+                <Td><Link onClick={() => {console.log("accept")}}>Accept</Link></Td>
+                <Td><Link onClick={() => {console.log("decline")}}>Decline</Link></Td>
+                {/* <Td><Link onClick={ () => Approve(id) }>Accept</Link></Td> */}
+                {/* <Td><Link onClick={ () => Decline(id) }>Decline</Link></Td> */}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
   );
 };
 
