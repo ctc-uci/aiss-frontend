eb503f5a48a8de49135ed70d8821f329cc9014fe
liannejl <liannejl@uci.edu>
2024-02-02 13:13:03 -0800

    Implements delete functionality for rows without conflicting ids with other tables

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,16 +1,41 @@
-import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Button } from '@chakra-ui/react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Button,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  ModalFooter,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { NPOBackend } from '../../utils/auth_utils';
 import CreateEventForm from '../../components/CreateEventForm/CreateEventForm';
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
 
 export default function Catalog() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [tableData, setTableData] = useState([]);
   const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
+  const [hoveredRow, setHoveredRow] = useState(null);
+  const [isEditFormOpen, setIsEditFormOpen] = useState(false);
+  const [deleteItemId, setDeleteItemId] = useState(null);
+  const [editData, setEditData] = useState([]);
 
   useEffect(() => {
     const fetchCatalogData = async () => {
       const response = await NPOBackend.get('/catalog');
       setTableData(response.data);
+      console.log(editData);
     };
 
     fetchCatalogData().catch(console.error);
@@ -20,10 +45,53 @@ export default function Catalog() {
     setIsCreateFormOpen(!isCreateFormOpen);
   };
 
+  const handleRowHover = index => {
+    setHoveredRow(index);
+  };
+
+  const handleRowLeave = () => {
+    setHoveredRow(null);
+  };
+
+  const handleEditForm = data => {
+    setEditData(data);
+    setIsEditFormOpen(!isEditFormOpen);
+    // populate form with data from setEditData (pass into CreateEventForm component as an event prop?)
+  };
+
+  const handleCloseEditForm = () => {
+    setIsEditFormOpen(!isEditFormOpen);
+  };
+
+  const handleDeleteClick = id => {
+    setDeleteItemId(id);
+    onOpen();
+  };
+
+  const handleConfirmDelete = async idToDelete => {
+    try {
+      await NPOBackend.delete(`/catalog/${idToDelete}`);
+
+      const response = await NPOBackend.get('/catalog');
+      setTableData(response.data);
+
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <Button onClick={openCreateForm}>Create</Button>
       {isCreateFormOpen && <CreateEventForm />}
+      <Modal id="editForm" isOpen={isEditFormOpen} onClose={handleCloseEditForm}>
+        <ModalContent>
+          <ModalBody>
+            <CreateEventForm />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
       <TableContainer>
         <Table variant="simple">
           <Thead>
@@ -37,19 +105,58 @@ export default function Catalog() {
             </Tr>
           </Thead>
           <Tbody>
-            {tableData.map(({ id, host, title, eventType, subject, description, year }) => (
-              <Tr key={id}>
+            {tableData.map(({ id, host, title, eventType, subject, description, year }, index) => (
+              <Tr key={id} onMouseEnter={() => handleRowHover(index)} onMouseLeave={handleRowLeave}>
                 <Td>{title}</Td>
                 <Td>{host}</Td>
                 <Td>{year}</Td>
                 <Td>{eventType}</Td>
                 <Td>{subject}</Td>
                 <Td>{description}</Td>
+                <Td>
+                  {hoveredRow === index && (
+                    <>
+                      <IconButton
+                        icon={
+                          <EditIcon
+                            onClick={() =>
+                              handleEditForm([
+                                id,
+                                title,
+                                host,
+                                year,
+                                eventType,
+                                subject,
+                                description,
+                              ])
+                            }
+                          />
+                        }
+                      />
+                      <IconButton icon={<DeleteIcon />} onClick={() => handleDeleteClick(id)} />
+                    </>
+                  )}
+                </Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
       </TableContainer>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirm Delete</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Are you sure you want to delete this row?</ModalBody>
+          <ModalFooter>
+            <ModalFooter>
+              <Button onClick={() => handleConfirmDelete(deleteItemId)}>Delete</Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
