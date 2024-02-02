13b11c50004dd7d7bf14658156ac6ba637d7763b
liannejl <liannejl@uci.edu>
2024-02-02 12:07:02 -0800

    Adds Create button to show CreateEventForm  on catalog page

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,9 +1,11 @@
-import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Button } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { NPOBackend } from '../../utils/auth_utils';
+import CreateEventForm from '../../components/CreateEventForm/CreateEventForm';
 
 export default function Catalog() {
   const [tableData, setTableData] = useState([]);
+  const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
 
   useEffect(() => {
     const fetchCatalogData = async () => {
@@ -14,32 +16,40 @@ export default function Catalog() {
     fetchCatalogData().catch(console.error);
   }, []);
 
+  const openCreateForm = () => {
+    setIsCreateFormOpen(!isCreateFormOpen);
+  };
+
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Title</Th>
-            <Th>Host</Th>
-            <Th>Cohort Type</Th>
-            <Th>Event Type</Th>
-            <Th>Subject</Th>
-            <Th>Description</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {tableData.map(({ id, host, title, eventType, subject, description, year }) => (
-            <Tr key={id}>
-              <Td>{title}</Td>
-              <Td>{host}</Td>
-              <Td>{year}</Td>
-              <Td>{eventType}</Td>
-              <Td>{subject}</Td>
-              <Td>{description}</Td>
+    <div>
+      <Button onClick={openCreateForm}>Create</Button>
+      {isCreateFormOpen && <CreateEventForm />}
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Title</Th>
+              <Th>Host</Th>
+              <Th>Cohort Type</Th>
+              <Th>Event Type</Th>
+              <Th>Subject</Th>
+              <Th>Description</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody>
+            {tableData.map(({ id, host, title, eventType, subject, description, year }) => (
+              <Tr key={id}>
+                <Td>{title}</Td>
+                <Td>{host}</Td>
+                <Td>{year}</Td>
+                <Td>{eventType}</Td>
+                <Td>{subject}</Td>
+                <Td>{description}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
