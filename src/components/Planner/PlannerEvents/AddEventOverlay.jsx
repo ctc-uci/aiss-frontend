bd73c67c974d4ce2bec44e0ce864b9dc7cb01f1b
Philip Jian <psjian@uci.edu>
2024-02-14 13:39:52 -0800

    added create event form inside day planner
    Co-authored-by: chloecheng8 <chloecheng8@users.noreply.github.com>
    Co-authored-by: kristenyee <kristenyee@users.noreply.github.com>
    Co-authored-by: Subin Kim <SubinQKim@users.noreply.github.com>

--- src/components/Planner/PlannerEvents/AddEventOverlay.jsx
+++ src/components/Planner/PlannerEvents/AddEventOverlay.jsx
@@ -1,17 +1,21 @@
 import { Stack, Text, Button } from '@chakra-ui/react';
-import s from '../PlannerLayout.module.css';
+// import s from '../PlannerLayout.module.css';
+import CreateEventForm from '../../CreateEventForm/CreateEventForm';
 
 // eslint-disable-next-line react/prop-types
 const AddEventOverlay = ({ setOverlayIsVisible }) => {
   return (
-    <div id={s['add-event-overlay']}>
+    // <div id={s['add-event-overlay']}>
+    <div>
       <Text fontSize="30px" marginBottom="1rem">
         MM/DD/YYYY | Location
       </Text>
-      <div className={s['add-event-container']}>
+      {/* <div className={s['add-event-container']}> */}
+      <div>
         <div>
-          <Text fontSize="1.25rem">Event Information</Text>
-        </div>
+          <CreateEventForm/>
+          {/* <Text fontSize="1.25rem">Event Information</Text> */}
+        </div>  
         <Stack spacing={2} justifyContent="right" direction="row">
           <Button
             paddingX="1.5rem"
--- src/components/Planner/PlannerLayout.jsx
+++ src/components/Planner/PlannerLayout.jsx
@@ -1,7 +1,9 @@
+import {
+  VStack
+} from '@chakra-ui/react'
 import s from './PlannerLayout.module.css';
 import PlannerTimeline from './PlannerTimeline/PlannerTimeline';
-// import PlannerEvents from './PlannerEvents/PlannerEvents';
-// import AddEventOverlay from './PlannerEvents/AddEventOverlay';
+import PlannerEvents from './PlannerEvents/PlannerEvents';
 import { PlannerContextProvider } from './PlannerContext';
 import Catalog from '../../pages/Catalog/Catalog.jsx'
 
@@ -10,8 +12,10 @@ const PlannerLayout = () => {
     <main id={s['planner-page-layout']}>
       <PlannerContextProvider>
         <PlannerTimeline />
-        {/* <AddEventOverlay /> */}
-        <Catalog />
+        <VStack>
+          <PlannerEvents/>
+          <Catalog />
+        </VStack>
       </PlannerContextProvider>
     </main>
   );
