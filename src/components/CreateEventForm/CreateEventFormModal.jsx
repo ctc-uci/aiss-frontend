0dd452ffb8bb7c0008792b7710a86462668b42e5
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2024-02-15 16:26:50 -0800

    Add edit, delete, create event to catalog (#37)
    * Adds Create button to show CreateEventForm  on catalog page
    
    * Implements delete functionality for rows without conflicting ids with other tables
    
    * Implements edit functionality for the create event form
    
    * Made create form a modal too. Still facing edit button issue
    
    * Fixed the edit button bug
    
    * moved the modals to separate components
    
    ---------
    
    Co-authored-by: liannejl <liannejl@uci.edu>
    Co-authored-by: michellelin1 <michellelin810@gmail.com>

---
+++ src/components/CreateEventForm/CreateEventFormModal.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+import CreateEventForm from './CreateEventForm';
+
+const CreateEventFormModal = ({isOpen, onClose, eventData, setModified}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="full">
+      <ModalOverlay />
+      <ModalContent margin={0} rounded="none">
+        <ModalCloseButton />
+        <ModalBody>
+          { eventData ? (
+            <CreateEventForm eventData={eventData} setModified={setModified} closeModal={onClose} />
+          ) : (
+            <CreateEventForm setModified={setModified} closeModal={onClose} />
+          )}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+
+CreateEventFormModal.propTypes = {
+  eventData: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    host: PropTypes.string,
+    year: PropTypes.number,
+    eventType: PropTypes.string,
+    subject: PropTypes.string,
+    description: PropTypes.string
+  }),
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  setModified: PropTypes.func.isRequired,
+}
+
+CreateEventForm.defaultProps = {
+  eventData: undefined,
+};
+
+
+export default CreateEventFormModal;
