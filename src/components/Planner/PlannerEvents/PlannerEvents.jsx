da476eeddce7270e87332f04994a2acc0b40f615
subinqkim <subinlk@uci.edu>
2024-02-16 15:54:38 -0800

    Put the catalog component in PlannerEvents component + fixed useState management for button visibility

--- src/components/Planner/PlannerEvents/PlannerEvents.jsx
+++ src/components/Planner/PlannerEvents/PlannerEvents.jsx
@@ -1,50 +1,53 @@
-import s from '../PlannerLayout.module.css';
-import {
-  Text,
-  Button,
-} from '@chakra-ui/react';
-import { AddIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
+import s from '../PlannerLayout.module.css';
+import { Text, Button } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
 import AddEventOverlay from './AddEventOverlay';
-// import CreateEventForm from '../../CreateEventForm/CreateEventForm';
+import Catalog from '../../../pages/Catalog/Catalog';
 
 const PlannerEvents = () => {
   const [overlayIsVisible, setOverlayIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleCreateEventClick = () => {
+    setOverlayIsVisible(!overlayIsVisible);
+    setIsVisible(!isVisible);
+  };
 
   return (
-    <div>
     <div id={s['planner-events-container']}>
-    {/* <div> */}
-      {overlayIsVisible && <AddEventOverlay setOverlayIsVisible={setOverlayIsVisible} />}
+      {overlayIsVisible && <AddEventOverlay setOverlayIsVisible={handleCreateEventClick} />}
       <div id={s['planner-browse']}>
-      {/* <div> */}
-        <Text fontSize="1.875rem" marginBottom="1rem">
-          Add Event to Day
-        </Text>
-        <Button
-          onClick={() => {
-            setOverlayIsVisible(true);
-          }}
-          className={s['create-event-button']}
-          backgroundColor="blackAlpha.500"
-          _hover={{
-            backgroundColor: 'blackAlpha.400',
-          }}
-          padding="1.5rem"
-          justifyContent="space-between"
-          alignItems="center"
-          marginBottom="1rem"
-        >
-          <Text fontSize="1.125rem" color="white">
-            Create New Event
-          </Text>
-          {/* <CreateEventForm/> */}
-          <AddIcon color="white" />
-        </Button>
+        {isVisible && ( 
+          <>
+            <Text fontSize="1.875rem" marginBottom="1rem">
+              Add Event to Day
+            </Text>
+            <Button
+              onClick={handleCreateEventClick}
+              className={s['create-event-button']}
+              backgroundColor="blackAlpha.500"
+              _hover={{
+                backgroundColor: 'blackAlpha.400',
+              }}
+              padding="1.5rem"
+              justifyContent="space-between"
+              alignItems="center"
+              marginBottom="1rem"
+              size="lg"
+            >
+              <Text fontSize="1.125rem" color="white">
+                Create New Event
+              </Text>
+              <AddIcon color="white" />
+            </Button>
+          </>
+        )}
+        <Catalog />
         <hr className={s['header-divider']} />
       </div>
     </div>
-  </div>    
   );
 };
+
 export default PlannerEvents;
--- src/components/Planner/PlannerLayout.jsx
+++ src/components/Planner/PlannerLayout.jsx
@@ -1,21 +1,14 @@
-import {
-  VStack
-} from '@chakra-ui/react'
 import s from './PlannerLayout.module.css';
 import PlannerTimeline from './PlannerTimeline/PlannerTimeline';
 import PlannerEvents from './PlannerEvents/PlannerEvents';
 import { PlannerContextProvider } from './PlannerContext';
-import Catalog from '../../pages/Catalog/Catalog.jsx'
 
 const PlannerLayout = () => {
   return (
     <main id={s['planner-page-layout']}>
       <PlannerContextProvider>
         <PlannerTimeline />
-        <VStack>
-          <PlannerEvents/>
-          <Catalog />
-        </VStack>
+        <PlannerEvents/>
       </PlannerContextProvider>
     </main>
   );
