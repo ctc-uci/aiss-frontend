f988c91554da308da1479591695d9556b6c830db
Sean Fong <seancfong@gmail.com>
2024-02-18 12:56:16 +0000

    Touch up styles on drawer

--- src/pages/NotificationSandbox/NotificationSandbox.jsx
+++ src/pages/NotificationSandbox/NotificationSandbox.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Button,
+  Container,
   Drawer,
   DrawerBody,
   DrawerHeader,
@@ -24,11 +25,19 @@ const NotificationSandbox = () => {
         <Button ref={buttonRef} colorScheme="teal" onClick={onOpen}>
           <BellIcon />
         </Button>
-        <Drawer size="lg" isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={buttonRef}>
+        <Drawer
+          size="lg"
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={buttonRef}
+        >
           <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Notifications</DrawerHeader>
+          <DrawerContent borderLeftRadius="2rem">
+            <Container m="0" maxW="none" display="flex" alignItems="center" borderBottomColor="gray.300" borderBottomWidth="1px" pl="6">
+              <DrawerCloseButton fontSize="0.5rem" p="0" w="6" h="6" borderRadius="100rem" borderColor="black" borderWidth="1px" position="relative" top="0" left="0" />
+              <DrawerHeader fontSize="2xl">Notifications</DrawerHeader>
+            </Container>
             <DrawerBody padding="0">
               <Notifications />
             </DrawerBody>
