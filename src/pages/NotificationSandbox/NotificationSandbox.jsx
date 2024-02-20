beaf4f547f24f7b7d8efebf26e01565819cf70c1
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2024-02-20 15:38:40 -0800

    Complete Notification Page Functionality (Frontend) (#39)
    * Add notification drawer and grouping
    
    * Add account pending component
    
    * Added callbacks to approval
    
    * Add finishing touches
    
    * Touch up styles on drawer
    
    * Added toasts
    
    * Prettier formatting
    
    ---------
    
    Co-authored-by: Sean Fong <seancfong@gmail.com>
    Co-authored-by: ThatMegamind <92563733+ThatMegamind@users.noreply.github.com>

---
+++ src/pages/NotificationSandbox/NotificationSandbox.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Button,
+  Container,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import Notifications from '../../components/Notifications/Notifications';
+import { BellIcon } from '@chakra-ui/icons';
+
+const NotificationSandbox = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const buttonRef = React.useRef();
+
+  return (
+    <div>
+      <h1>Notification Sandbox</h1>
+      {/* TODO: Move Notification button and drawer to navbar once layout component is pushed */}
+      <>
+        <Button ref={buttonRef} colorScheme="teal" onClick={onOpen}>
+          <BellIcon />
+        </Button>
+        <Drawer
+          size="lg"
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={buttonRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent borderLeftRadius="2rem">
+            <Container
+              m="0"
+              maxW="none"
+              display="flex"
+              alignItems="center"
+              borderBottomColor="gray.300"
+              borderBottomWidth="1px"
+              pl="6"
+            >
+              <DrawerCloseButton
+                fontSize="0.5rem"
+                p="0"
+                w="6"
+                h="6"
+                borderRadius="100rem"
+                borderColor="black"
+                borderWidth="1px"
+                position="relative"
+                top="0"
+                left="0"
+              />
+              <DrawerHeader fontSize="2xl">Notifications</DrawerHeader>
+            </Container>
+            <DrawerBody padding="0">
+              <Notifications />
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </>
+    </div>
+  );
+};
+
+export default NotificationSandbox;
