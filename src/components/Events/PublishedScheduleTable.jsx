7672ef59e04c0917b5c3ec2db91c1faa0528d0a6
liannejl <liannejl@uci.edu>
2024-02-28 13:40:56 -0800

    Attempt at calculating and inserting breaks into published schedule

--- src/components/Events/PublishedScheduleTable.jsx
+++ src/components/Events/PublishedScheduleTable.jsx
@@ -9,7 +9,7 @@ import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Box } from '@chakra-ui
 
 const PublishedScheduleTable = ({ season }) => {
   const [eventsInDay, setEventsInDay] = useState([]);
-  const [prevEvent, setPrevEvent] = useState();
+  // const [prevEvent, setPrevEvent] = useState();
   const seasonType = season.split(' ')[0];
   const seasonYear = season.split(' ')[1];
 
@@ -24,14 +24,39 @@ const PublishedScheduleTable = ({ season }) => {
   }, [seasonType, seasonYear]);
 
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const checkBreak = day => {
-    if (prevEvent && day.startTime > prevEvent.endTime) {
-      const eventData = [0, prevEvent.endTime, day.startTime, 'Break / Team Time'];
-      setPrevEvent(day);
-      return <Events eventData={eventData} />;
-    }
-    setPrevEvent(day);
-  };
+  // const checkBreak = day => {
+  //   if (prevEvent && day.startTime > prevEvent.endTime) {
+  //     const eventData = [0, prevEvent.endTime, day.startTime, 'Break / Team Time'];
+  //     setPrevEvent(day);
+  //     return <EventInfo eventData={eventData} />;
+  //   }
+  //   setPrevEvent(day);
+  // };
+
+  // const calculateBreaks = events => {
+  //   const breaks = [];
+  //   for (let i = 0; i < events.length - 1; i++) {
+  //     const currentEvent = events[i];
+  //     const nextEvent = events[i + 1];
+
+  //     if (currentEvent.endTime < nextEvent.startTime) {
+  //       breaks.push({
+  //         startTime: currentEvent.endTime,
+  //         endTime: nextEvent.startTime,
+  //         type: 'Break / Team Time',
+  //       });
+  //     }
+  //   }
+  //   return breaks;
+  // };
+
+  // const breaks = calculateBreaks(
+  //   eventsInDay.flatMap(item => [
+  //     ...item.data,
+  //     { startTime: item.day.startTime, endTime: item.day.endTime },
+  //   ]),
+  // );
+
   return (
     <Box>
       <TableContainer>
@@ -46,7 +71,7 @@ const PublishedScheduleTable = ({ season }) => {
             {eventsInDay.map(item => (
               <Tr key={item.day.id} verticalAlign={'top'}>
                 {/* <Td><Events/></Td> ?? there is a break */}
-                {checkBreak(item)}
+                {/* {checkBreak(item)} */}
                 <Td>
                   <EventInfo
                     eventDate={item.day.eventDate}
@@ -61,6 +86,18 @@ const PublishedScheduleTable = ({ season }) => {
                   <Events eventData={item.data} location={item.day.location} />
                 </Td>
               </Tr>
+
+              // {breaks.map((breakItem, index) => (
+              //   <Tr key={`break-${index}`} verticalAlign=["top"]>
+              //     <Td>
+              //       <EventInfo
+              //         startTime={breakItem.startTime}
+              //         endTime={breakTime.endTime}
+              //         type={breakItem.type}
+              //       />
+              //     </Td>
+              //   </Tr>
+              // ))}
             ))}
           </Tbody>
         </Table>
