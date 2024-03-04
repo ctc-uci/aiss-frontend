ccf46c7053a12420013933594d8b4dd4531ee521
liannejl <liannejl@uci.edu>
2024-03-04 13:00:18 -0800

    Added breaks in between events for each day

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,28 +1,38 @@
 import DailyEvent from './DailyEvent.jsx';
 import PropTypes from 'prop-types';
 
-import {
-    Grid,
-} from '@chakra-ui/react';
+import { Grid } from '@chakra-ui/react';
 
-
-const Events = ( {eventData, location} ) => {
-    return (
-
-        <Grid gap={3}>
-            {eventData.map(item => (
-                <DailyEvent
-                    key={item.id}
-                    startTime={item.startTime}
-                    endTime={item.endTime}
-                    eventTitle={item.title}
-                    location={location}
-                />
-            ))}
-        </Grid>
-            
-    );
-  };
+const Events = ({ eventData, location }) => {
+  const eventDataWithBreaks = [];
+  for (let i = 0; i < eventData.length - 1; i++) {
+    const currentEvent = eventData[i];
+    const nextEvent = eventData[i + 1];
+    eventDataWithBreaks.push(currentEvent);
+    if (currentEvent.endTime < nextEvent.startTime) {
+      eventDataWithBreaks.push({
+        startTime: currentEvent.endTime,
+        endTime: nextEvent.startTime,
+        title: 'Break / Team Time',
+        location: 'N/A',
+      });
+      eventDataWithBreaks.push(nextEvent);
+    }
+  }
+  return (
+    <Grid gap={3}>
+      {eventDataWithBreaks.map(item => (
+        <DailyEvent
+          key={item.id}
+          startTime={item.startTime}
+          endTime={item.endTime}
+          eventTitle={item.title}
+          location={item.location == null ? location : null}
+        />
+      ))}
+    </Grid>
+  );
+};
 
 Events.propTypes = {
   eventData: PropTypes.array.isRequired,
--- src/components/Events/PublishedScheduleTable.jsx
+++ src/components/Events/PublishedScheduleTable.jsx
@@ -9,8 +9,6 @@ import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Box } from '@chakra-ui
 
 const PublishedScheduleTable = ({ season }) => {
   const [eventsInDay, setEventsInDay] = useState([]);
-  // const [prevEvent, setPrevEvent] = useState(null);
-  const [breaks, setBreaks] = useState([]);
   const seasonType = season.split(' ')[0];
   const seasonYear = season.split(' ')[1];
 
@@ -20,79 +18,11 @@ const PublishedScheduleTable = ({ season }) => {
         `/published-schedule/season?season=${seasonType}&year=${seasonYear}`,
       );
       setEventsInDay(data);
-      // setPrevEvent(data.length > 0 ? data[0].day : null);
     };
     renderTable();
   }, [seasonType, seasonYear]);
 
-  useEffect(() => {
-    const calculateBreaks = () => {
-      const allEvents = eventsInDay.flatMap(item => [
-        ...item.data,
-        { startTime: item.day.startTime, endTime: item.day.endTime },
-      ]);
-      const calculatedBreaks = [];
-      for (let i = 0; i < allEvents.length - 1; i++) {
-        const currentEvent = allEvents[i];
-        const nextEvent = allEvents[i + 1];
-
-        if (currentEvent.endTime < nextEvent.startTime) {
-          calculatedBreaks.push({
-            startTime: currentEvent.endTime,
-            endTime: nextEvent.startTime,
-            eventTitle: 'Break / Team Time',
-          });
-        }
-      }
-      return calculatedBreaks;
-    };
-
-    setBreaks(calculateBreaks());
-  }, [eventsInDay]);
-
-
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  // const checkBreak = day => {
-  //   if (
-  //     prevEvent !== null &&
-  //     new Date(day.startTime).getTime() > new Date(prevEvent.endTime).getTime()
-  //   ) {
-  //     // const eventData = [0, prevEvent.endTime, day.startTime, 'Break / Team Time'];
-  //     setPrevEvent(day);
-  //     console.log('there should be a break here');
-  //     return true;
-  //   } else {
-  //     console.log("there isn't a break");
-  //     setPrevEvent(day);
-  //     console.log(day);
-  //     console.log(prevEvent);
-  //     return false;
-  //   }
-  // };
-
-  // const calculateBreaks = events => {
-  //   const breaks = [];
-  //   for (let i = 0; i < events.length - 1; i++) {
-  //     const currentEvent = events[i];
-  //     const nextEvent = events[i + 1];
-
-  //     if (currentEvent.endTime < nextEvent.startTime) {
-  //       breaks.push({
-  //         startTime: currentEvent.endTime,
-  //         endTime: nextEvent.startTime,
-  //         type: 'Break / Team Time',
-  //       });
-  //     }
-  //   }
-  //   return breaks;
-  // };
-
-  // const breaks = calculateBreaks(
-  //   eventsInDay.flatMap(item => [
-  //     ...item.data,
-  //     { startTime: item.day.startTime, endTime: item.day.endTime },
-  //   ]),
-  // );
 
   return (
     <Box>
@@ -117,31 +47,10 @@ const PublishedScheduleTable = ({ season }) => {
                     notes={item.day.notes}
                   />
                 </Td>
+
                 <Td>
                   <Events eventData={item.data} location={item.day.location} />
-                  {breaks.map((breakItem, index) => (
-                    <Tr key={`break-${index}`} verticalAlign={['top']}>
-                      <Td>
-                        <Events
-                          eventData={item.data.concat(breaks)}
-                        />
-                      </Td>
-                    </Tr>
-                  ))}
                 </Td>
-
-                {/* {checkBreak(item.day) ? (
-                  <Td>
-                    <Events
-                      eventData={{
-                        id: 0,
-                        startTime: prevEvent.endTime,
-                        endTime: item.day.startTime,
-                        eventTitle: 'Break / Team Time',
-                      }}
-                    />
-                  </Td>
-                ) : null} */}
               </Tr>
             ))}
           </Tbody>
