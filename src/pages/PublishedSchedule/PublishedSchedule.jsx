b26ce4d1f79a74b3a8dff71d1460df86e8b4b627
kristenyee <66435886+kristenyee@users.noreply.github.com>
2024-03-04 20:57:53 -0800

    42 published schedule match high fidelity (#52)
    * fixed date and time formats
    
    * matched design and added buttons (not functional yet)
    
    * fixed size of select
    
    * sorted season and changed default season
    
    * reversed sorting of seasons
    
    * whatever changes you guys wanted
    
    * corrected timezone to display correct date
    
    ---------
    
    Co-authored-by: Kristen Yee <kristenyee@dhcp-v016-054.mobile.uci.edu>
    Co-authored-by: Kristen Yee <kristenyee@dhcp-v115-235.mobile.uci.edu>
    Co-authored-by: ThatMegamind <udongenius@hotmail.com>
    Co-authored-by: Kristen Yee <kristenyee@dhcp-v051-110.mobile.uci.edu>
    Co-authored-by: Kristen Yee <kristenyee@Kristens-MacBook-Air.local>
    Co-authored-by: michellelin1 <michellelin810@gmail.com>
    Co-authored-by: ThatMegamind <92563733+ThatMegamind@users.noreply.github.com>

--- src/pages/PublishedSchedule/PublishedSchedule.jsx
+++ src/pages/PublishedSchedule/PublishedSchedule.jsx
@@ -2,48 +2,88 @@ import { NPOBackend } from '../../utils/auth_utils.js';
 import PublishedScheduleTable from '../../components/Events/PublishedScheduleTable.jsx';
 
 import { useEffect, useState } from 'react';
-import {
-  Box,
-  Select,
-} from '@chakra-ui/react';
-
+import { Box, Select, Text } from '@chakra-ui/react';
 
 const PublishedSchedule = () => {
   // get data from database
   const [allSeasons, setAllSeasons] = useState([]);
   const [selectedSeason, setSelectedSeason] = useState('');
 
+  const getTodaySeason = () => {
+    let today = new Date();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let season;
+
+    if (month >= 3 && month <= 5) {
+      season = 'Spring';
+    } else if (month >= 9 && month <= 11) {
+      season = 'Fall';
+    } else if (month >= 6 && month <= 8) {
+      season = 'Summer';
+    } else {
+      season = 'Winter';
+    }
+    return season + ' ' + year.toString();
+  };
+
   useEffect(() => {
     const renderTable = async () => {
       const { data } = await NPOBackend.get('/published-schedule/all-seasons');
+      const index = data.indexOf(curSeason);
+      if (index !== -1) {
+        data.splice(index, 1);
+      }
+
+      const seasonOrder = ['Summer', 'Fall', 'Winter', 'Spring'];
+      data.sort((a, b) => {
+        // Compare years first
+        if (a.split(' ')[1] !== b.split(' ')[1]) {
+          return b.split(' ')[1] - a.split(' ')[1];
+        } else {
+          return seasonOrder.indexOf(a.split(' ')[0]) - seasonOrder.indexOf(b.split(' ')[0]);
+        }
+      });
       setAllSeasons(data);
     };
     renderTable();
-    
   }, []);
+
+  const curSeason = getTodaySeason();
 
   //update chakra table container accordingly
   return (
     <Box pt={10} pb={10} pl={100} pr={100}>
-        {/* season dropdown menu */}
-        <Select variant='unstyled' placeholder='All Seasons' onChange={() => setSelectedSeason(event.target.value)} width="20%">
-            {allSeasons.map(item => (
-              <option key={item} value={item}>{item}</option>
-            ))}
-        </Select> 
+      {selectedSeason != '' ? (
+        <Text fontSize="2.5vw" mb="-5vh" fontWeight="bold">
+          {selectedSeason}
+        </Text>
+      ) : (
+        <Text fontSize="2.5vw" mb="-5vh" fontWeight="bold">
+          {curSeason}
+        </Text>
+      )}
+      <Select
+        mb="3vh"
+        variant="unstyled"
+        placeholder={curSeason}
+        textColor="transparent"
+        onChange={() => setSelectedSeason(event.target.value)}
+        width="23%"
+      >
+        {allSeasons.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </Select>
 
-        {/* tables for each season */}
-        {selectedSeason != '' ? 
-          (<PublishedScheduleTable
-            season={selectedSeason}
-          />) :  
-          (allSeasons.map(item => (
-            <PublishedScheduleTable
-              key={item}
-              season={item}
-            />
-          )))
-        }
+      {/* tables for each season */}
+      {selectedSeason != '' ? (
+        <PublishedScheduleTable season={selectedSeason} />
+      ) : (
+        <PublishedScheduleTable season={curSeason} />
+      )}
     </Box>
   );
 };
