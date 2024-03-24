1bded46743a2bce1478a857041d08d23e94139f3
ThatMegamind <udongenius@hotmail.com>
2024-03-23 17:25:58 -0700

    re-formatting times

--- src/components/Events/PublishedScheduleTable.jsx
+++ src/components/Events/PublishedScheduleTable.jsx
@@ -24,15 +24,15 @@ const PublishedScheduleTable = ({ season }) => {
 
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-//   const formatDate = date => {
-//     let time = date.split(':');
-//     let hours = time[0];
-//     let mins = time[1];
-//     let ampm = hours >= 12 ? 'pm' : 'am';
-//     hours = hours % 12;
-//     hours = hours ? hours : 12;
-//     return `${hours}:${mins} ${ampm}`;
-//   };
+  const formatDate = date => {
+    let time = date.split(':');
+    let hours = time[0];
+    let mins = time[1];
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return `${hours}:${mins} ${ampm}`;
+  };
 
   const getUTCDate = (eventDate) => {
     const utcDate = new Date(eventDate);
@@ -56,8 +56,8 @@ const PublishedScheduleTable = ({ season }) => {
                   <EventInfo
                     eventDate={getUTCDate(item.day.eventDate).toLocaleDateString('en-US')}
                     day={dayNames[getUTCDate(item.day.eventDate).getDay()]}
-                    startTime={item.day.startTime}
-                    endTime={item.day.endTime}
+                    startTime={formatDate(item.day.startTime)}
+                    endTime={formatDate(item.day.endTime)}
                     location={item.day.location}
                     notes={item.day.notes}
                   />
