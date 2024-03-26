11222dbb13065262b01006d35b79c95cd47a46ad
michellelin1 <michellelin810@gmail.com>
2024-03-26 00:17:03 -0700

    missed a file

--- src/components/AddDayForm/AddDayForm.jsx
+++ src/components/AddDayForm/AddDayForm.jsx
@@ -17,72 +17,56 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { NPOBackend } from '../../utils/auth_utils';
 
-  const schema = yup.object({
-    date: yup.date().nullable().transform((curr, orig) => orig === '' ? null : curr).required('Date required'),
-    location: yup.string().required('Location required').max(50, 'Location exceeds 50 character limit'),
-    details: yup
-      .string()
-      .max(50, 'Details exceeds 50 character limit'),
+const schema = yup.object({
+  date: yup.date().nullable().transform((curr, orig) => orig === '' ? null : curr).required('Date required'),
+  location: yup.string().required('Location required').max(50, 'Location exceeds 50 character limit'),
+  details: yup
+    .string()
+    .max(50, 'Details exceeds 50 character limit'),
+});
+
+const AddDayForm = ({ onClose, onOpen, setDayId }) => {
+  const toast = useToast();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
-  const AddDayForm = ({ onClose, onOpen, setDayId }) => {
-    const toast = useToast();
-    const {
-      handleSubmit,
-      register,
-      formState: { errors },
-    } = useForm({
-      resolver: yupResolver(schema),
-    });
+  const submitData = async data => {
+    const { date, details, location } = data;
+    toast.closeAll();
+    try {
+      const payload = {
+        eventDate: date,
+        location: location,
+        notes: details,
+      };
 
-    const submitData = async data => {
-      const { date, details, location } = data;
-      toast.closeAll();
-      try {
-        const payload = {
-          eventDate: date,
-          location: location,
-          notes: details,
-        };
-
-        const response = await NPOBackend.post('/day/', payload);
-        if (response.status === 201) {
-          toast({
-            title: 'add day!',
-            description: `date: ${date}, description: ${details}, location: ${location}`,
-            status: 'success',
-            variant: 'subtle',
-            position: 'bottom',
-            containerStyle: {
-            mt: '6rem',
-            },
-            duration: 3000,
-            isClosable: true,
-          });
-          const id = response.data['id']
-          setDayId(id);
-          console.log(id);
-          onOpen(id);
-        } else {
-          toast({
-            title: 'Error Adding Day',
-            description: `date: ${date}, description: ${details}, location: ${location}`,
-            status: 'error',
-            variant: 'subtle',
-            position: 'bottom',
-            containerStyle: {
-            mt: '6rem',
-            },
-            duration: 3000,
-            isClosable: true,
-          });
-        }
-
-      } catch (error) {
-        console.log(error);
+      const response = await NPOBackend.post('/day/', payload);
+      if (response.status === 201) {
+        toast({
+          title: 'add day!',
+          description: `date: ${date}, description: ${details}, location: ${location}`,
+          status: 'success',
+          variant: 'subtle',
+          position: 'bottom',
+          containerStyle: {
+          mt: '6rem',
+          },
+          duration: 3000,
+          isClosable: true,
+        });
+        const id = response.data['id']
+        setDayId(id);
+        console.log(id);
+        onOpen(id);
+      } else {
         toast({
           title: 'Error Adding Day',
-          description: error.response ? error.response.data.message : 'An error occurred',
+          description: `date: ${date}, description: ${details}, location: ${location}`,
           status: 'error',
           variant: 'subtle',
           position: 'bottom',
@@ -93,75 +77,91 @@ import { NPOBackend } from '../../utils/auth_utils';
           isClosable: true,
         });
       }
+
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: 'Error Adding Day',
+        description: error.response ? error.response.data.message : 'An error occurred',
+        status: 'error',
+        variant: 'subtle',
+        position: 'bottom',
+        containerStyle: {
+        mt: '6rem',
+        },
+        duration: 3000,
+        isClosable: true,
+      });
     }
+  }
 
-    return (
-      <Box p="20px">
-        <Heading size="lg" textAlign="center" mb="8px" color="#2D3748">Add New Day</Heading>
-        <form onSubmit={handleSubmit(data => submitData(data))}>
-          <Box mb="4vh">
-            {/* DATE */}
-            <Box mb="15px">
-              <FormControl isInvalid={errors && errors.date} >
-                <FormLabel fontWeight="bold" color="gray.600">Date *</FormLabel>
-                <Input
-                  size="md"
-                  type="date"
-                  {...register('date')}
-                />
-                <FormErrorMessage>{errors.date && errors.date.message}</FormErrorMessage>
-              </FormControl>
-            </Box>
-
-            {/* LOCATION */}
-            <Box mb="15px">
-              <FormControl isInvalid={errors && errors.location} >
-                <FormLabel fontWeight="bold" color="gray.600">Location *</FormLabel>
-                <Input placeholder='Location' {...register('location')} />
-                <FormErrorMessage>{errors.location && errors.location.message}</FormErrorMessage>
-              </FormControl>
-            </Box>
-
-
-            {/* DETAILS */}
-            <Box mb="15px">
-              <FormControl isInvalid={errors && errors.details}>
-                <FormLabel fontWeight="bold" color="gray.600">Details</FormLabel>
-                <Textarea placeholder='Details' {...register('details')} />
-                <FormErrorMessage>
-                  {errors.details && errors.details.message}
-                </FormErrorMessage>
-              </FormControl>
-            </Box>
+  return (
+    <Box p="20px">
+      <Heading size="lg" textAlign="center" mb="8px" color="#2D3748">Add New Day</Heading>
+      <form onSubmit={handleSubmit(data => submitData(data))}>
+        <Box mb="4vh">
+          {/* DATE */}
+          <Box mb="15px">
+            <FormControl isInvalid={errors && errors.date} >
+              <FormLabel fontWeight="bold" color="gray.600">Date *</FormLabel>
+              <Input
+                size="md"
+                type="date"
+                {...register('date')}
+              />
+              <FormErrorMessage>{errors.date && errors.date.message}</FormErrorMessage>
+            </FormControl>
           </Box>
 
-          <Flex justifyContent="space-between">
-            <Button width="48%" size='lg' onClick={onClose}>
-                Cancel
-            </Button>
-            <Button
-              width="48%"
-              type='submit'
-              size='lg'
-              bgColor="#2c93d1"
-              color="white"
-              _hover={{ bgColor: '#1b6896' }}
-            >
-              + Add To Schedule
-            </Button>
-          </Flex>
-        </form>
-      </Box>
-    );
-  };
+          {/* LOCATION */}
+          <Box mb="15px">
+            <FormControl isInvalid={errors && errors.location} >
+              <FormLabel fontWeight="bold" color="gray.600">Location *</FormLabel>
+              <Input placeholder='Location' {...register('location')} />
+              <FormErrorMessage>{errors.location && errors.location.message}</FormErrorMessage>
+            </FormControl>
+          </Box>
 
-  AddDayForm.propTypes = {
-    onClose: PropTypes.func,
-    onOpen: PropTypes.func,
-    setDayId: PropTypes.func
-  };
-  AddDayForm.defaultProps = {
-    onClose: () => {},
-    onOpen: () => {},
-  };
-  export default AddDayForm;
+
+          {/* DETAILS */}
+          <Box mb="15px">
+            <FormControl isInvalid={errors && errors.details}>
+              <FormLabel fontWeight="bold" color="gray.600">Details</FormLabel>
+              <Textarea placeholder='Details' {...register('details')} />
+              <FormErrorMessage>
+                {errors.details && errors.details.message}
+              </FormErrorMessage>
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Flex justifyContent="space-between">
+          <Button width="48%" size='lg' onClick={onClose}>
+              Cancel
+          </Button>
+          <Button
+            width="48%"
+            type='submit'
+            size='lg'
+            bgColor="#2c93d1"
+            color="white"
+            _hover={{ bgColor: '#1b6896' }}
+          >
+            + Add To Schedule
+          </Button>
+        </Flex>
+      </form>
+    </Box>
+  );
+};
+
+AddDayForm.propTypes = {
+  onClose: PropTypes.func,
+  onOpen: PropTypes.func,
+  setDayId: PropTypes.func
+};
+AddDayForm.defaultProps = {
+  onClose: () => {},
+  onOpen: () => {},
+};
+export default AddDayForm;
