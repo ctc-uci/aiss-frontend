8cd020e7672bb5d2f369952d1a46cfaa1697052e
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2024-04-16 13:10:35 -0700

    Bug Batch 3 Fixes (#74)
    * Create a pull trequest for branch 73-Bug-Batch-3
    
    * Bug fixes
    
    * Fixed bug in published schedule where season selected was incorrect
    
    * modal message changes
    
    ---------
    
    Co-authored-by: Lana Ramadan <lanamramadan@gmail.com>
    Co-authored-by: ThatMegamind <udongenius@hotmail.com>
    Co-authored-by: ThatMegamind <92563733+ThatMegamind@users.noreply.github.com>

--- src/components/Catalog/DeleteEventModal/DeleteEventModal.jsx
+++ src/components/Catalog/DeleteEventModal/DeleteEventModal.jsx
@@ -26,12 +26,12 @@ const DeleteEventModal = ({ isOpen, onClose, deleteItemId, setDataShouldRevalida
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Confirm Delete</ModalHeader>
+        <ModalHeader>Remove Event?</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>Are you sure you want to delete this row?</ModalBody>
+        <ModalBody>Are you sure you want to delete this event? You cannot undo this action afterward.</ModalBody>
         <ModalFooter>
-          <Button onClick={() => handleConfirmDelete(deleteItemId)}>Delete</Button>
-          <Button onClick={onClose}>Cancel</Button>
+          <Button onClick={onClose} mr={2}>Cancel</Button>
+          <Button onClick={() => handleConfirmDelete(deleteItemId)} colorScheme='red'>Delete</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
