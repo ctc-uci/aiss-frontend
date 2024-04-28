430cf0b280839f5282ad0488c2d4215ccb25d790
michellelin1 <michellelin810@gmail.com>
2024-04-27 21:44:23 -0700

    refresh data on day planner close

--- src/components/Events/PublishedScheduleTable.jsx
+++ src/components/Events/PublishedScheduleTable.jsx
@@ -38,7 +38,8 @@ const PublishedScheduleTable = ({ season, allSeasons }) => {
       renderTable();
       setShouldDataRevalidate(false);
     }
-  }, [dataShouldRevalidate])
+    console.log(dataShouldRevalidate);
+  }, [dataShouldRevalidate, renderTable])
 
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
